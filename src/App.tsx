f3a560fe2b961445646cdafe3036c6aae3793b6a
Max Sebso <maxsebso@gmail.com>
2020-07-17 01:07:10 -0700

    Installed the following dependencies: axios, react-router-dom, redux, react-redux, material-ui/core, and jest. Also the homepage now displays a welcome message to the participants.
    Remove .DS_Store files and add IntelliJ local config files to gitignore

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To C4C Jumpstart!</h1>
+      <h2 className="Congrats">If you are seeing this then you have correctly installed your project!</h2>
+    </div>
+  );
+}
+
+export default App;
