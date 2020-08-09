dfb302881ffd67e5ac5c1659c3affd1d517c7b02
Max Sebso <maxsebso@gmail.com>
2020-08-08 23:04:06 -0700

    Added Static Elements for a "Create Blog Post" through html without react. Content of page is a simple form for creating a blog post.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Blog from './components/BlogPost';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
   document.getElementById('root'),
 );
