230e13f14354dffa8e352aca7ecfcf3c22ae9dd9
Max Sebso <maxsebso@gmail.com>
2020-09-16 22:39:00 -0400

    Update typescript and linting config
    Added in Travis-CI configuration to enforce linting before official
    pushes
    
    Add new npm commands and project files to README

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import './css/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Welcome To C4C Jumpstart!</h1>
-      <h2 className="Congrats">If you are seeing this then you have correctly installed your project!</h2>
+      <h2 className="Congrats">
+        If you are seeing this then you have correctly installed your project!
+      </h2>
     </div>
   );
-}
+};
 
 export default App;
